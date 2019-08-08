72854a8e55c3af524c8508e425032ea9a2d9e37e
assiddeek <aic.it2015@gmail.com>
2019-08-08 15:35:26 +0700

    Initial

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
 <?php
 require_once('./vendor/autoload.php');
-//
-Namespace
+//Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-$channel_token =
-'DYKKHgUhUFUOGn1tPbP1UGEJFV/Ww+MsAJ8liQVFG5RkZ6D/EryVeymFXbDpn+zciZiMIJ3mx0lAltZjwKX3mDu50NVNb5itvd7pP8w+pXzWogTAjgUVC1BiO8ibanzREjPMJ/GJZK14yTclSGs8/QdB04t89/1O/w1cDnyilFU=';
+$channel_token ='DYKKHgUhUFUOGn1tPbP1UGEJFV/Ww+MsAJ8liQVFG5RkZ6D/EryVeymFXbDpn+zciZiMIJ3mx0lAltZjwKX3mDu50NVNb5itvd7pP8w+pXzWogTAjgUVC1BiO8ibanzREjPMJ/GJZK14yTclSGs8/QdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'c0236b5dc114d1e52688890efdd16b93';
-//
-Get message from Line API
+//Get message from Line API
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 if (!is_null($events['events'])) {
-//
-Loop through each event
+//Loop through each event
 foreach ($events['events'] as $event) {
-//
-Line API send a lot of event type, we interested in message only.
+//Line API send a lot of event type, we interested in message only.
     if ($event['type'] == 'message') {
         switch($event['message']['type']) {
             case 'text':
