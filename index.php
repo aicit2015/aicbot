348c8c9f3e951047e3155cd89af2c6d33f3fcd35
assiddeek <aic.it2015@gmail.com>
2019-08-08 15:44:24 +0700

    Initial

--- index.php
+++ index.php
@@ -20,12 +20,20 @@ foreach ($events['events'] as $event) {
                 $replyToken = $event['replyToken'];
                 // Reply message
                 $respMessage = 'Hello, your message is '. $event['message']['text'];
-                $httpClient = new CurlHTTPClient($channel_token);
-                $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-                $textMessageBuilder = new TextMessageBuilder($respMessage);
-                $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+               
+                
+            case 'image':
+                $messageID = $event['message']['id'];
+                $respMessage = 'Hello, your image ID is '. $messageID;
+            break;
+            default:
+                $respMessage = 'Please send image only';
             break;
             }
+            $httpClient = new CurlHTTPClient($channel_token);
+            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+            $textMessageBuilder = new TextMessageBuilder($respMessage);
+            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
         }
     }
 }
